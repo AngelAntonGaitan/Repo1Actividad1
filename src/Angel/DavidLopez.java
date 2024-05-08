e449d00d77af1248c45dc9b64a8f2bf4d9735dd8
David Lopez <dlopezgomez17@gmail.com>
2024-05-08 11:18:09 +0200

    David Lopez Gomez

---
+++ src/Angel/DavidLopez.java
@@ -0,0 +1,16 @@
+package Angel;
+
+public class DavidLopez {
+
+	public static void decirNombre() {
+		System.out.println("Hola soy David Lopez");
+			}
+
+	public static void main(String[] args) {
+		
+		DavidLopez persona = new DavidLopez();
+	
+		persona.decirNombre();
+	}
+	
+}
