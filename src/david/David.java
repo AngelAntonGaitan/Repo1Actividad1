57b0e4e78bc667cf065b919416bc726d75260a13
Angel <angelyavanna@hotmail.com>
2024-05-08 14:37:13 +0200

    manager arregla un fallo en java

--- src/david/David.java
+++ src/david/David.java
@@ -1,6 +1,10 @@
 package david;
 
 public class David {
+	
+	public static void decirNombre() {
+		System.out.println("Hola soy David");
+			}
 
 	public static void main(String[] args) {
 		System.out.println("Hola soy David");
